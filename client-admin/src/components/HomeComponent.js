978b0b523368e1dbfb14d643e60c12293821c36f
TamPham-coder <Phamchitam965@gmail.com>
2026-03-19 17:24:17 +0700

    fix nested repo

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img
+          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDZwcm92ZHYxZ290ZzJqdGtyeXh3aDVhYnV0anFxYnBsMWZ4dXFqeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xTiTnqUxyWbsAXq7Ju/giphy.gif"
+          width="800px"
+          height="600px"
+          alt=""
+        />
+      </div>
+    );
+  }
+}
+
+export default Home;
